31e5b23f45195b731b67ca51b08f24b4f591ff42
arimoura <arimateia.moura@armazemparaiba.com.br>
2016-02-26 12:06:54 -0300

    adicionando aluno

--- deploy/aluno.sql
+++ deploy/aluno.sql
@@ -1,15 +1,17 @@
--- Deploy escola:schema_escola to pg
+-- Deploy escola:aluno to pg
+-- requires: schema_escola
 
 BEGIN;
 
 -- XXX Add DDLs here.
 
-create tabela  escola.aluno;
+create table  escola.aluno ( 
 id bigserial primary key,
 nome varchar(40) not null,
 cpf char(11),
 dt_inclusao date default now(),
 fl_ativo char(1)
 );
+
 
 COMMIT;
---
+++ verify/aluno.sql
@@ -0,0 +1,20 @@
+-- Verify escola:aluno on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+select 
+id,
+nome,
+cpf,
+dt_inclusao,
+fl_ativo
+from escola.aluno
+where
+false;
+
+select pg_catalog.has_table_privilege('ESCOLA.ALUNO','SELECT');
+
+
+ROLLBACK;
